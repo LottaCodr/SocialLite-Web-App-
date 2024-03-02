2c76cc3c2acc57631665a235f37b976b0cd49d40
King-Lota <lottanna47@gmail.com>
2024-03-02 03:01:11 +0100

    Auth Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import AuthLayout from './_auth/forms/AuthLayout';
+import SignUpForms from './_auth/forms/SignUpForms';
 import SigninForms from './_auth/forms/SigninForms';
+import RootLayout from './_root/RootLayout';
 import { Home } from './_root/pages';
 import './globals.css';
 import {Routes, Route } from "react-router-dom";
@@ -8,12 +11,18 @@ const App = () => {
     <main className='flex h-screen'>
         <Routes>
             {/* public routes */}
+
+            <Route element={<AuthLayout/>}>
         
         <Route path='/sign-in' element={<SigninForms/>}/>
+        <Route path='/sign-up' element={<SignUpForms/>}/>
+        </Route>
 
 
             {/* private Routes */}
+            <Route element={<RootLayout/>}>
             <Route index element={<Home/>}/>
+            </Route>
         </Routes>
     </main>
   )
