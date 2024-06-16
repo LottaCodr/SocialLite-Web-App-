df266cbe8d663a5fda8b77ba8d2cab73a11085cb
King-Lota <lottanna47@gmail.com>
2024-06-16 16:22:15 +0100

    Sign oput user implemented

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,22 @@
+import BottomBar from "@/components/shared/BottomBar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import Topbar from "@/components/shared/Topbar"
+
+import { Outlet } from "react-router-dom"
 
 function RootLayout() {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar/>
+      <LeftSidebar/>
+
+      <section className="flex flex-1 h-full">
+        <Outlet/>
+
+      </section>
+
+      <BottomBar/>
+    </div>
   )
 }
 
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -19,3 +19,9 @@ export const useSignInAccount = () => {
         
     });
 }
+export const useSignOutAccount = () => {
+    return useMutation ({
+        mutationFn: () => signOutAccount
+        
+    });
+}
