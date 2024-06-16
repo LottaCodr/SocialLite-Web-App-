3702b66291375f851a9fec2569fff50a58662642
King-Lota <lottanna47@gmail.com>
2024-06-16 23:25:35 +0100

    New Pages and Routes Added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AuthLayout from './_auth/forms/AuthLayout';
 import SignUpForms from './_auth/forms/SignUpForms';
 import SigninForms from './_auth/forms/SigninForms';
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, LikedPost, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import './globals.css';
 import {Routes, Route } from "react-router-dom";
 
@@ -27,6 +27,15 @@ const App = () => {
             {/* private Routes */}
             <Route element={<RootLayout/>}>
             <Route index element={<Home/>}/>
+            <Route path='/explore' element={<Explore/>}/>
+            <Route path='/saved' element={<Saved/>}/>
+            <Route path='/all-users' element={<AllUsers/>}/>
+            <Route path='/create-post' element={<CreatePost/>}/>
+            <Route path='/update-post/:id' element={<EditPost/>}/>
+            <Route path='/posts/:id' element={<PostDetails/>}/>
+            <Route path='/profile/:id/*' element={<Profile/>}/>
+            <Route path='/update-profile/:id' element={<UpdateProfile/>}/>
+            <Route path='/liked-post/:id' element={<LikedPost/>}/>
             </Route>
         </Routes>
 
