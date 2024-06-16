d9c44a74b58ee2992e8576926ceeaff5dc64b602
King-Lota <lottanna47@gmail.com>
2024-06-16 19:57:08 +0100

    Sidebar link mapping

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,52 +1,62 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
-import { useEffect } from 'react';
-import { useUserContext } from '@/context/AuthContext';
-
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
 
 const LeftSidebar = () => {
-    const { mutate: signOut, isSuccess} = useSignOutAccount();
-    const navigate = useNavigate();
-    const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
 
-    useEffect(() => {
-        if(isSuccess) navigate(0);
-    }, [isSuccess])
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
   return (
     <nav className="leftsidebar">
-        <div className="flex flex-col gap-11" >
-        <Link to='/' className='flex gap-3 items-center'>
-                <img
-                src='/assets/images/logo.svg'
-                alt='logo'
-                width={170}
-                height={36}
-                />
-            </Link>
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
 
+        <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+          <img
+            src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-18 rounded-full"
+          />
 
-            <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
-                <img
-                src= {user.imageUrl || '/assets/images/profile-placeholder.svg'}
-                alt='profile'
-                className='h-14 w-18 rounded-full'
-                />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
 
-                <div className='flex flex-col'>
-                    <p className='body-bold'>
-                        {user.name}
-                    </p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
 
-                    <p className='small-regular text-light-3'>
-                        @{user.username}
-                    </p>
-                </div>
-            </Link>
-
-            <ul className='flex flex-col gap-6'></ul>
-        </div>
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            return (
+              <li key={link.label} className="lefsidebar-link">
+                <NavLink to={link.route} className='flex gap-4 items-center p-4' >
+                    <img src={link.imgURL} 
+                    alt={link.label}
+                    className = 'group-hover: invert-white'
+                    />
+                    {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
